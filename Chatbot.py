65632a11eb8dee680ea288468738c49547e5c733
wint3000 <winton_pierson@yahoo.com>
2025-08-24 16:16:21 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -10,7 +10,7 @@ import os
 import streamlit as st
 
 st.set_page_config(page_title="Winton's ChatBot", page_icon="💬")
-st.title("💬 Chatbot")
+st.title("😄 Winton's Chatbot")
 
 # --- Sidebar controls ---
 with st.sidebar:
